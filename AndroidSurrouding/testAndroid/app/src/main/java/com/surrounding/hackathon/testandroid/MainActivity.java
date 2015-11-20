55fef7f58a6e805287c64ebc7f7230ff3809b672
Trần Minh Tuấn <tuantmtb@gmail.com>
2015-11-20 20:43:10 +0700

    second post again

--- AndroidSurrouding/testAndroid/app/src/main/java/com/surrounding/hackathon/testandroid/MainActivity.java
+++ AndroidSurrouding/testAndroid/app/src/main/java/com/surrounding/hackathon/testandroid/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //test first code
+    // test again
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
