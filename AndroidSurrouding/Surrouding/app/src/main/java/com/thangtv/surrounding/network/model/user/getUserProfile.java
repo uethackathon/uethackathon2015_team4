b1e14e63f71430b4fb5c5c63c029409c43e7b72c
tannv_57 <nvantan94@gmail.com>
2015-11-22 07:19:08 +0700

    update project surrounding

---
+++ AndroidSurrouding/Surrouding/app/src/main/java/com/thangtv/surrounding/network/model/user/getUserProfile.java
@@ -0,0 +1,75 @@
+package com.thangtv.surrounding.network.model.user;
+
+/**
+ * Created by Ngo Van Tan on 11/22/2015.
+ */
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("org.jsonschema2pojo")
+public class GetUserProfile {
+    @SerializedName("status")
+    @Expose
+    private int status;
+    @SerializedName("data")
+    @Expose
+    private Data data;
+    @SerializedName("message")
+    @Expose
+    private String message;
+
+    /**
+     *
+     * @return
+     * The status
+     */
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     *
+     * @param status
+     * The status
+     */
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    /**
+     *
+     * @return
+     * The data
+     */
+    public Data getData() {
+        return data;
+    }
+
+    /**
+     *
+     * @param data
+     * The data
+     */
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    /**
+     *
+     * @return
+     * The message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     *
+     * @param message
+     * The message
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
